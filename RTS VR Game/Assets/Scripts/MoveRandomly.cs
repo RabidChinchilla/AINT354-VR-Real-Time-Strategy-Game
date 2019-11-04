adb2ba4d27c0dcb3661534e0999f670223146e93
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-11-04 22:18:09 +0000

    random movement for demo

---
+++ RTS VR Game/Assets/Scripts/MoveRandomly.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveRandomly : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    public float timerForNewPath;
+    bool inCoRoutine = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inCoRoutine)
+        {
+            StartCoroutine(doSomething());
+        }
+    }
+
+    Vector3 getNewRandomPosition()
+    {
+        float x = Random.Range(-20, 20);
+        float z = Random.Range(-20, 20);
+
+        Vector3 pos = new Vector3(x, 0, z);
+
+        return pos;
+    }
+
+    void GetNewPath()
+    {
+        navMeshAgent.SetDestination(getNewRandomPosition());
+    }
+
+    IEnumerator doSomething()
+    {
+        inCoRoutine = true;
+        yield return new WaitForSeconds(timerForNewPath);
+        GetNewPath();
+        inCoRoutine = false;
+    }
+}
