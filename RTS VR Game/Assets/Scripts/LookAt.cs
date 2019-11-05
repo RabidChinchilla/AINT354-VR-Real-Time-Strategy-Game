98832f7aa7b221ab07392e1730faff8b13d310f6
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-11-05 15:30:45 +0000

    look at target script done

---
+++ RTS VR Game/Assets/Scripts/LookAt.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+
+    private Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            target = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            target = null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null)
+        {
+            transform.LookAt(target);
+        }
+    }
+}
