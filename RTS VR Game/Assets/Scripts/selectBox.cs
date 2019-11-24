9e2dafd56f31af1367ca1eea7f7c94d52cd891ba
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-11-24 10:36:57 +0000

    working on selection box

---
+++ RTS VR Game/Assets/Scripts/selectBox.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selectBox : MonoBehaviour
+{
+    [HideInInspector]
+    private RectTransform selectSquareImage;
+
+    Vector3 startPos;
+    Vector3 endPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selectSquareImage.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            {
+                startPos = hit.point;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            selectSquareImage.gameObject.SetActive(false);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (!selectSquareImage.gameObject.activeInHierarchy)
+            {
+                selectSquareImage.gameObject.SetActive(true);
+            }
+
+            endPos = Input.mousePosition;
+
+
+        }
+    }
+}
