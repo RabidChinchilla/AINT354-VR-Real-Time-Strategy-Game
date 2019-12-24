e8ce8f063a20bc270c944ad9be71fa13d4971a1b
Gracaeus <tictacasian@gmail.com>
2019-12-24 20:05:47 +0000

    Unit Movement
    Unit Movemnet on click began whilst following the nav mesh

---
+++ RTS VR Game/Assets/Scripts/moveUnits.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class moveUnits : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    NavMeshPath path;
+    public float timerForNewPath;
+    bool inCoRoutine = false;
+    Vector3 target;
+    bool validPath;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        path = new NavMeshPath();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inCoRoutine)
+        {
+            StartCoroutine(doSomething());
+        }
+    }
+
+    Vector3 getNewRandomPosition()
+    {
+        float x = Random.Range(-20, 20);
+        float z = Random.Range(-20, 20);
+
+        Vector3 pos = new Vector3(x, 0, z);
+
+        return pos;
+    }
+
+    void GetNewPath()
+    {
+        target = getNewRandomPosition();
+        navMeshAgent.SetDestination(target);
+    }
+
+    IEnumerator doSomething()
+    {
+        inCoRoutine = true;
+        yield return new WaitForSeconds(timerForNewPath);
+        GetNewPath();
+        validPath = navMeshAgent.CalculatePath(target, path);
+        if (!validPath) Debug.Log("Found an invalid path");
+
+        while (!validPath)
+        {
+            yield return new WaitForSeconds(0.01f); //prevents crash
+            GetNewPath();
+            validPath = navMeshAgent.CalculatePath(target, path);
+        }
+        inCoRoutine = false;
+    }
+}
