f7be8b3b3145b387e3428f6c907b35a10ec70c5c
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2019-12-31 18:03:44 +0000

    got raycast hitting the ground layer and registering it

--- RTS VR Game/Assets/Scripts/carpetBomb.cs
+++ RTS VR Game/Assets/Scripts/carpetBomb.cs
@@ -9,37 +9,23 @@ public class carpetBomb : MonoBehaviour
 
     void Start()
     {
-        RaycastHit hit;
-        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
 
-        if (Physics.Raycast(ray, out hit))
-        {
-            Transform objectHit = hit.transform;
-
-            // Do something with the object that was hit by the raycast.
-        }
     }
 
         void Update()
     {
 
-        // Bit shift the index of the layer (8) to get a bit mask
-        int layerMask = 1 << 8;
-
-        // This would cast rays only against colliders in layer 8.
-        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-        layerMask = ~layerMask;
-
         RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        // Does the ray intersect any objects excluding the layer
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, 1 << LayerMask.NameToLayer("GroundLayer")))
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            //Debug.DrawRay(ray * hit.distance, Color.yellow);
             Debug.Log("Did Hit");
         }
         else
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            //Debug.DrawRay(ray * 1000, Color.white);
             Debug.Log("Did not Hit");
         }
     }
--- RTS VR Game/Assets/Scripts/commandPost.cs
+++ RTS VR Game/Assets/Scripts/commandPost.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//using NUnit.Framework;
 
 public class commandPost : MonoBehaviour
 {
     public int health = 2000;
-    public int resources = 200;
+    public int resources;
     public Text resourceCounter;
+
+    //void OnEnable()
+    //{
+    //    Assert.IsNotNull(resources, "ScoreText is not set!");
+    //    Assert.IsNotNull(resourceCounter, "ScoreText is not set!");
+    //}
 
     // Start is called before the first frame update
     void Start()
     {
+        resources = 200;
         resourceCounter.text = "Resources: " + resources;
     }
 
