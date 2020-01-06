0c6357dd49912df036d26157798a79a44d0b5c75
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2020-01-06 00:44:30 +0000

    most base code done

---
+++ RTS VR Game/Assets/Scripts/showBuildingButtons.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showBuildingButtons : MonoBehaviour
+{
+    public GameObject button1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            // If player clicked on a selectable unit
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                Debug.Log("Unit clicked: " + hit.transform.gameObject.name);
+                if (hit.transform.gameObject.tag == "CommandBuilding")
+                {
+                    button1.SetActive(true);
+                    return;
+                }
+            }
+
+        }
+
+    }
+}
