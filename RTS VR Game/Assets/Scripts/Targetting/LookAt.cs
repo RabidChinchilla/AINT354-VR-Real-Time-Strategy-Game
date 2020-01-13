941704ee8c293699b5b2f6c7f072cd77dcf2d835
Ben Gearing <33025242+RabidChinchilla@users.noreply.github.com>
2020-01-13 16:45:10 +0000

    look at now uses sphere casting

--- RTS VR Game/Assets/Scripts/Targetting/LookAt.cs
+++ RTS VR Game/Assets/Scripts/Targetting/LookAt.cs
@@ -8,12 +8,31 @@ public class LookAt : MonoBehaviour
     private Transform target;
     private GameObject targetObject;
     private int damage;
+    private float currentHitDistance;
 
 
     // Start is called before the first frame update
     void Awake()
     {
         damage = gameObject.GetComponent<unitAttributes>().damage;
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.SphereCast(transform.position, 5f, transform.forward, out hit, 20))
+        {
+            currentHitDistance = hit.distance;
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Look at Did Hit");
+        }
+        else
+        {
+            currentHitDistance = hit.distance;
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 70, Color.white);
+            Debug.Log("Look at Did not Hit");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -59,5 +78,12 @@ public class LookAt : MonoBehaviour
         yield return new WaitForSecondsRealtime(1.5f);
         StartCoroutine(Attack());
         //Debug.Log("Damage is: "+damage);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Debug.DrawLine(transform.position, transform.position + transform.forward * currentHitDistance);
+        Gizmos.DrawWireSphere(transform.position + transform.forward * currentHitDistance, 5f);
     }
 }
